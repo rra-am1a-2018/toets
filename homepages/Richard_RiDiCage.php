8df71b06c3f708055facd01d402f446873c27baa
RiDiCage <rvdkooij@live.nl>
2019-06-28 13:12:04 +0200

    saved name

--- homepages/Richard_RiDiCage.php
+++ homepages/Richard_RiDiCage.php
@@ -16,30 +16,22 @@
         <table class="table table-striped table-dark">
             <thead>
                 <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">LLnr</th>
+                <th scope="col">voornaam</th>
+                <th scope="col">tussenvoegsel</th>
+                <th scope="col">achternaam</th>
+                <th scope="col">afstudeerrichting</th>
+                <th scope="col">avatar</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                </tr>
-                <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-                </tr>
-                <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
+                <th scope="row">310426</th>
+                <td>Richard</td>
+                <td>van de</td>
+                <td>Kooij</td>
+                <td></td>
+                <td></td>
                 </tr>
             </tbody>
         </table>
