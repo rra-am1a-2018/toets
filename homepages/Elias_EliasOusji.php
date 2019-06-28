155f071e5cff19406f091412fd69ad8a3f26191e
Elias Ousji <eliasousji01@gmail.com>
2019-06-28 14:06:25 +0200

    Pagina toegevoegd 2

---
+++ homepages/Elias_EliasOusji.php
@@ -0,0 +1,56 @@
+<?php
+  echo "Mijn homepage";
+?>
+
+<!DOCTYPE html>
+<html lang="nl-NL">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Proeftoets - AM1A</title>
+    </head>
+    <body>
+      <div class="container-fluid">
+        <h1>Toets - AM1A</h1>
+        <table class="table table-bordered table-dark">
+  <thead>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Leerlingnummer:</th>
+      <td>322774</td>
+    </tr>
+    <tr>
+      <th scope="row">Voornaam:</th>
+      <td>Elias</td>
+    </tr>
+    <tr>
+      <th scope="row">Tussenvoegsel:</th>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">Achternaam:</th>
+      <td>Ousji</td>
+    </tr>
+    <tr>
+      <th scope="row">Afstudeerrichting:</th>
+      <td>Applicatie ontwikkelaar</td>
+    </tr>
+    <tr>
+      <th scope="row">Avatar:</th>
+      <td><img src="../img/robin.jpg" width="50px" height="50px"></td>
+    </tr>
+  </tbody>
+</table>
+<a href="../index.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Back to index</a>
+      </div>
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
